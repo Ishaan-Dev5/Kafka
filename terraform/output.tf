2eff151f86921dd45c02b5127fd9440aef5688cd
Ishaan-Dev5 <ishaanaggarwal32@gmail.com>
2025-06-11 12:20:54 +0530

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,21 +1,49 @@
-output "bastion_public_ip" {
-  value = aws_instance.bastionhost.public_ip
-}
-
-
 output "vpc_id" {
-  value = aws_vpc.main.id
+  description = "The ID of the VPC"
+  value       = aws_vpc.main.id
 }
 
-output "public_subnets" {
-  value = aws_subnet.public[*].id
+output "public_subnet_1_id" {
+  description = "Public Subnet 1 ID"
+  value       = aws_subnet.public_subnet_1.id
 }
 
-output "private_subnets" {
-  value = aws_subnet.private[*].id
+output "public_subnet_2_id" {
+  description = "Public Subnet 2 ID"
+  value       = aws_subnet.public_subnet_2.id
 }
 
-output "nat_gateway_id" {
-  value = aws_nat_gateway.nat.id
+output "private_subnet_1_id" {
+  description = "Private Subnet 1 ID"
+  value       = aws_subnet.private_subnet_1.id
 }
 
+output "private_subnet_2_id" {
+  description = "Private Subnet 2 ID"
+  value       = aws_subnet.private_subnet_2.id
+}
+
+output "bastionhost_public_ip" {
+  description = "Public IP of the bastion host"
+  value       = aws_instance.instance.public_ip
+}
+
+output "private_instance_1_private_ip" {
+  description = "Private IP of Kafka private instance 1"
+  value       = aws_instance.private_instance_1.private_ip
+}
+
+output "private_instance_2_private_ip" {
+  description = "Private IP of Kafka private instance 2"
+  value       = aws_instance.private_instance_2.private_ip
+}
+
+output "kafka_nlb_dns_name" {
+  description = "DNS name of the Kafka Network Load Balancer"
+  value       = aws_lb.kafka_nlb.dns_name
+}
+
+output "kafka_nlb_target_group_arn" {
+  description = "Kafka Target Group ARN"
+  value       = aws_lb_target_group.kafka_tg.arn
+}
